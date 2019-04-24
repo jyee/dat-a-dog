35a150e791faff8032f96149461f6655de2bc365
Jason Yee <jason.yee@datadoghq.com>
2019-04-24 09:44:38 -0700

    Fix custom metric name, streamline tagging

--- app/dat-a-dog.py
+++ app/dat-a-dog.py
@@ -14,23 +14,23 @@ patch_all()
 app = Flask(__name__)
 env = os.environ.get("DD_SERVICE_ENV")
 conf = os.environ.get("DD_CONF_NAME")
+tags = ["env:" + env, "conference:" + conf]
 
 @app.route("/")
 def index():
-  tags = ["env:" + env, "conference:" + conf, "page:index"]
+  statsd.increment("dat_a_dog.pageviews", tags = tags + ["page:index"])
   return render_template("dat-a-dog.html", image = get_image(), conference = conf)
 
 @app.route("/is-dog/<is_dog>")
 def count_dog(is_dog):
   # Count page views.
-  tags = ["env:" + env, "conference:" + conf, "page:count_dog"]
-  statsd.increment("dat-a-dog.pageviews", tags = tags)
+  statsd.increment("dat_a_dog.pageviews", tags = tags + ["page:count_dog"])
 
   if is_dog == "yes":
-    statsd.increment("dat-a-dog.dogs", tags = tags)
+    statsd.increment("dat_a_dog.counted.dogs", tags = tags)
     message = "Such woof! Very dog! MOAR?"
   else:
-    statsd.increment("dat-a-dog.not-dogs", tags = tags)
+    statsd.increment("dat_a_dog.counted.not_dogs", tags = tags)
     message = "Not doge. I can haz another?"
 
   return render_template("dat-a-dog.html", image = get_image(), conference = conf, message = message)
@@ -38,10 +38,12 @@ def count_dog(is_dog):
 
 def get_image():
   if random.randint(0,1):
+    statsd.increment("dat_a_dog.generated.dogs", tags = tags)
     r = requests.get("https://dog.ceo/api/breeds/image/random")
     data = r.json()
     return data["message"]
   else:
+    statsd.increment("dat_a_dog.generated.not_dogs", tags = tags)
     r = requests.get("https://api.thecatapi.com/v1/images/search")
     data = r.json()
     return data[0]["url"]
